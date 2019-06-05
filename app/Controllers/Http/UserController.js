6211ef6d3c3cfffce6a42fe52c7db062d826c557
Henrique Tavares <ihenrits@gmail.com>
2019-06-05 00:02:02 -0300

    Cadastro de Usuario

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+  async store ({ request }) {
+    // const data = request.all()
+    const data = request.only(['username', 'email', 'password'])
+
+    const user = await User.create(data)
+
+    return user
+  }
+}
+
+module.exports = UserController
