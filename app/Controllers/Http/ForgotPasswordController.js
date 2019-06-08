cc21531ec3c19ddcb0a0224a91dac84236a7a6e3
Henrique Tavares <ihenrits@gmail.com>
2019-06-07 23:41:00 -0300

    Enviando email

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -13,6 +14,21 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('ihenrits@gmail.com', 'Henrique Tavares')
+            .subject('Recuperação de senha')
+        }
+      )
     } catch (err) {
       return response
         .status(err.status)
