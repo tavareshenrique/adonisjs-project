fac3b85df6e1b7989db38b220cbb47d0cb3bba95
Henrique Tavares <ihenrits@gmail.com>
2019-06-07 23:53:15 -0300

    Resetando Senha

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -33,6 +34,35 @@ class ForgotPasswordController {
       return response
         .status(err.status)
         .send({ error: { message: 'Algo não deu certo, esse e-mail existe?' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'O token de recuperação está expirado.' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Algo deu errado ao resetar sua senha' } })
     }
   }
 }
